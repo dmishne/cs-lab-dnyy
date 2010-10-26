33ff4528efc5bdc16d7eb9279456357496b0c008
geffen.nir <geffen.nir@ee6e6aaf-207a-3dc3-f7a4-2c6dd9a5b256>
2010-10-26 13:30:17 +0000

    Nir checking svn

--- trunk/HelloWorld/src/hello/CHello.java
+++ trunk/HelloWorld/src/hello/CHello.java
@@ -8,7 +8,7 @@ public class CHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("Checking for Yotam");
+		System.out.println("Checking"); // nir's editiom
 		
 		
 	}
