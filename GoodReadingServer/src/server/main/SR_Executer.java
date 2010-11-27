e06535d5d3bfb9ededc8f98004eca29b55c6ec9e
geffen.nir <geffen.nir@ee6e6aaf-207a-3dc3-f7a4-2c6dd9a5b256>
2010-11-27 13:03:10 +0000

    SR_Executer - java class file

---
+++ GoodReadingServer/src/server/main/SR_Executer.java
@@ -0,0 +1,56 @@
+package server.main;
+
+public class SR_Executer implements Runnable
+{
+	/*signleton*/
+	private static SR_Executer obj;
+	private boolean sleeping;
+	/*signleton*/
+	SR_Executer GetInstance()
+	{
+		/* TODO add constructor args */
+		if(obj == null)
+			init();
+		return obj;
+		
+	}
+	
+	private SR_Executer()
+	{
+	//todo constructor
+	}
+	private void init()
+	{
+		obj=new SR_Executer();
+		sleeping=true;
+		new Thread(obj).start();
+	}
+	public void run()
+	{
+		SR_Client_Entry Work;
+		try {
+			while (true)
+			{
+				if(SR_Standby_Unit.GetInstance().isEmpty())
+					sleeping = true;
+				while(sleeping)
+						wait();
+				Work=SR_Standby_Unit.GetInstance().getEntryFromQueue();
+				/*handle entry from standby unit*/
+				
+			}
+		} 
+		catch (InterruptedException e) 
+		{
+			System.out.println("Server fail, can't 'wait' in func run via SR_Executer");
+			e.printStackTrace();
+		}
+	}
+	
+	public void Notify()
+	{
+		if(sleeping)
+			sleeping=false;
+		notifyAll();
+	}
+}
