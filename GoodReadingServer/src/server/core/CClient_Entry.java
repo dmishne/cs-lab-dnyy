4ca760361c561c8e6efa7c910ef2f5cf28f0e691
geffen.nir <geffen.nir@ee6e6aaf-207a-3dc3-f7a4-2c6dd9a5b256>
2010-11-27 15:54:14 +0000

    another small change, added string session id as a key

--- GoodReadingServer/src/server/core/CClient_Entry.java
+++ GoodReadingServer/src/server/core/CClient_Entry.java
@@ -8,6 +8,7 @@ public class CClient_Entry implements Serializable {
 	String m_type; /* type of request */
 	Map <String, String> m_args; /* arguments for request - may change from request to request*/
 	java.net.InetAddress m_IP;
+	String m_sessionID;
 	Object m_client; /*TODO check about client relevancy */
 	
 	boolean isLogin()
