11694005da350176917587dbfdb4b29bd7b02eb2
geffen.nir <geffen.nir@ee6e6aaf-207a-3dc3-f7a4-2c6dd9a5b256>
2010-11-27 11:43:06 +0000

    Add class file - SR_Client_Entry.java
    added functions and attributes, need to decide oncorrect IP format*

---
+++ GoodReadingServer/src/server/main/SR_Client_Entry.java
@@ -0,0 +1,18 @@
+package server.main;
+
+import java.io.Serializable;
+import java.util.Map;
+
+@SuppressWarnings("serial")
+public class SR_Client_Entry implements Serializable {
+	String type; /* type of request */
+	Map <String, String> args; /* arguments for request - may change from request to request*/
+	java.net.InetAddress IP;
+	
+	boolean isLogin()
+	{
+		if ( type.equals("Login"))
+			return true;
+		return false;
+	}	
+}
