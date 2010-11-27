f6fed01ae95456244d4fa5490668ff82aafbc84d
geffen.nir <geffen.nir@ee6e6aaf-207a-3dc3-f7a4-2c6dd9a5b256>
2010-11-27 13:05:57 +0000

    updated variable ConnectionToClient client

--- GoodReadingServer/src/server/main/SR_Client_Entry.java
+++ GoodReadingServer/src/server/main/SR_Client_Entry.java
@@ -8,6 +8,7 @@ public class SR_Client_Entry implements Serializable {
 	String type; /* type of request */
 	Map <String, String> args; /* arguments for request - may change from request to request*/
 	java.net.InetAddress IP;
+	ConnectionToClient client;
 	
 	boolean isLogin()
 	{
