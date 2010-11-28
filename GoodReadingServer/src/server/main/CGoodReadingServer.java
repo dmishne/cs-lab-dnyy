8d4dbfd576ea5255fbed43049cc0c8571d13d769
geffen.nir <geffen.nir@ee6e6aaf-207a-3dc3-f7a4-2c6dd9a5b256>
2010-11-28 15:52:30 +0000

    base initialization written, might need updates from further modules

--- GoodReadingServer/src/server/main/CGoodReadingServer.java
+++ GoodReadingServer/src/server/main/CGoodReadingServer.java
@@ -1,5 +1,12 @@
 package server.main;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import server.core.CExecuter;
+import server.core.CStandbyUnit;
+import server.db.CDBInteractionGenerator;
+
 public class CGoodReadingServer {
 
 	/**
@@ -7,10 +14,22 @@ public class CGoodReadingServer {
 	 */
 	public static void main(String[] args) {
 		/*
-		 *  TODO:
-		 *  
+		 *  TODO: start up server,
+		 *  		first comes DB
+		 *  		second comes in the CStandbyUnit, Executer SHOULD go after standby unit, for he calls for an instance of it, however there is no limitation currently, it will justmake some of the code redundant to call it first
+		 *			next we should ??  
 		 */
-
+		CDBInteractionGenerator.GetInstance().ServerUpdateLog("Server Started loading at "+ (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime())));
+		CStandbyUnit.GetInstance();
+		CExecuter.GetInstance();
+	
+		//at last, we check that everthing is working
+		if(CDBInteractionGenerator.GetInstance() != null && CStandbyUnit.GetInstance() != null && CExecuter.GetInstance() != null)
+			CDBInteractionGenerator.GetInstance().ServerUpdateLog("Server online at "+ (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime())));
+		else 
+			CDBInteractionGenerator.GetInstance().ServerUpdateLog("Server failed startup at "+ (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime())));
+		
+		/* TODO: what next? */
 	}
 
 }
