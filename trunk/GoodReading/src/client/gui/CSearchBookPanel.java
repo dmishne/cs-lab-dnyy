bf6807e47b9d8b9ad5168d9b85657d0f81d9e880
daniel.mishne <daniel.mishne@ee6e6aaf-207a-3dc3-f7a4-2c6dd9a5b256>
2010-12-14 08:46:22 +0000

    Client: GUI: Added CSearchResultPanel, currently using stubbed information.

--- trunk/GoodReading/src/client/gui/CSearchBookPanel.java
+++ trunk/GoodReading/src/client/gui/CSearchBookPanel.java
@@ -18,10 +18,17 @@ import javax.swing.border.BevelBorder;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 
+import client.common.CClientConnector;
+
 public class CSearchBookPanel extends JPanel implements ActionListener{
 
+
+	public enum SBPDecision{
+		BACK, SEARCH
+	}
+	
 	private static final long serialVersionUID = 1L;
-	private JButton m_jButton_back = null;
+	private JButton m_jButton_back_SBP = null;
 	private JLabel m_jLabel_title_SBP = null;
 	private JLabel m_jLabel_Author_SBP = null;
 	private JLabel m_jLabel_Title_SBP = null;
@@ -37,11 +44,27 @@ public class CSearchBookPanel extends JPanel implements ActionListener{
 	private JTextField m_jTextField_Labels_SBP = null;
 	private JComboBox m_jComboBox_Languege_SBP = null;
 	private JComboBox m_jComboBox_Topics_SBP = null;
+	private JButton m_jButton_Search_SBP = null;
+	private SBPDecision m_lastChoice = SBPDecision.BACK;  //  @jve:decl-index=0:
+	
+	/**
+	 * @return the m_lastChoice
+	 */
+	public SBPDecision getLastChoice() {
+		return m_lastChoice;
+	}
+
+	/**
+	 * @param mLastChoice the m_lastChoice to set
+	 */
+	public void setLastChoice(SBPDecision mLastChoice) {
+		m_lastChoice = mLastChoice;
+	}
 
 	/**
 	 * This is the default constructor
 	 */
-	public CSearchBookPanel() {
+	public CSearchBookPanel() throws Exception{
 		super();
 		initialize();
 	}
@@ -51,7 +74,7 @@ public class CSearchBookPanel extends JPanel implements ActionListener{
 	 * 
 	 * @return void
 	 */
-	private void initialize() {
+	private void initialize() throws Exception {
 		m_jLabel_Labels_SBP = new JLabel();
 		m_jLabel_Labels_SBP.setText("Labels:");
 		m_jLabel_Labels_SBP.setSize(new Dimension(160, 40));
@@ -92,12 +115,12 @@ public class CSearchBookPanel extends JPanel implements ActionListener{
 		m_jLabel_title_SBP.setText("Search Book");
 		m_jLabel_title_SBP.setLocation(new Point(0, 60));
 		m_jLabel_title_SBP.setSize(new Dimension(700, 60));
-		m_jLabel_title_SBP.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+		m_jLabel_title_SBP.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
 		m_jLabel_title_SBP.setFont(new Font("Freestyle Script", Font.BOLD, 48));
 		this.setSize(700, 600);
 		this.setLayout(null);
 		this.setPreferredSize(new Dimension(700, 600));
-		this.add(getM_jButton_back(), null);
+		this.add(getM_jButton_back_SBP(), null);
 		this.add(m_jLabel_title_SBP, null);
 		this.add(m_jLabel_Author_SBP, null);
 		this.add(m_jLabel_Title_SBP, null);
@@ -113,23 +136,24 @@ public class CSearchBookPanel extends JPanel implements ActionListener{
 		this.add(getM_jTextField_Labels_SBP(), null);
 		this.add(getM_jComboBox_Languege_SBP(), null);
 		this.add(getM_jComboBox_Topics_SBP(), null);
+		this.add(getM_jButton_Search_SBP(), null);
 	}
 
 	/**
-	 * This method initializes m_jButton_back	
+	 * This method initializes m_jButton_back_SBP	
 	 * 	
 	 * @return javax.swing.JButton	
 	 */
-	private JButton getM_jButton_back() {
-		if (m_jButton_back == null) {
-			m_jButton_back = new JButton();
-			m_jButton_back.setPreferredSize(new Dimension(73, 27));
-			m_jButton_back.setLocation(new Point(28, 18));
-			m_jButton_back.setText("Back");
-			m_jButton_back.setSize(new Dimension(73, 27));
-			m_jButton_back.addActionListener(this);
+	private JButton getM_jButton_back_SBP() {
+		if (m_jButton_back_SBP == null) {
+			m_jButton_back_SBP = new JButton();
+			m_jButton_back_SBP.setPreferredSize(new Dimension(73, 27));
+			m_jButton_back_SBP.setLocation(new Point(94, 480));
+			m_jButton_back_SBP.setText("Back");
+			m_jButton_back_SBP.setSize(new Dimension(208, 34));
+			m_jButton_back_SBP.addActionListener(this);
 		}
-		return m_jButton_back;
+		return m_jButton_back_SBP;
 	}
 
 	/**
@@ -207,9 +231,9 @@ public class CSearchBookPanel extends JPanel implements ActionListener{
 	 * 	
 	 * @return javax.swing.JComboBox	
 	 */
-	private JComboBox getM_jComboBox_Languege_SBP() {
+	private JComboBox getM_jComboBox_Languege_SBP() throws Exception {
 		if (m_jComboBox_Languege_SBP == null) {
-			m_jComboBox_Languege_SBP = new JComboBox();
+			m_jComboBox_Languege_SBP = new JComboBox(CClientConnector.getInstance().getLangages());
 			m_jComboBox_Languege_SBP.setLocation(new Point(200, 235));
 			m_jComboBox_Languege_SBP.setSize(new Dimension(400, 30));
 		}
@@ -221,9 +245,9 @@ public class CSearchBookPanel extends JPanel implements ActionListener{
 	 * 	
 	 * @return javax.swing.JComboBox	
 	 */
-	private JComboBox getM_jComboBox_Topics_SBP() {
+	private JComboBox getM_jComboBox_Topics_SBP() throws Exception {
 		if (m_jComboBox_Topics_SBP == null) {
-			m_jComboBox_Topics_SBP = new JComboBox();
+			m_jComboBox_Topics_SBP = new JComboBox(CClientConnector.getInstance().getTopics());
 			m_jComboBox_Topics_SBP.setSize(new Dimension(400, 30));
 			m_jComboBox_Topics_SBP.setLocation(new Point(200, 355));
 		}
@@ -232,11 +256,31 @@ public class CSearchBookPanel extends JPanel implements ActionListener{
 
 	public void actionPerformed(ActionEvent ae) {
 		Object source = ae.getSource();
-		if(source == m_jButton_back)
+		if(source == m_jButton_back_SBP)
 		{
+			this.setLastChoice(SBPDecision.BACK);
 			this.setVisible(false);
 		}
-		
+		else if(source == m_jButton_Search_SBP)
+		{
+			this.setLastChoice(SBPDecision.SEARCH);
+			this.setVisible(false);
+		}
+	}
+
+	/**
+	 * This method initializes m_jButton_Search_SBP	
+	 * 	
+	 * @return javax.swing.JButton	
+	 */
+	private JButton getM_jButton_Search_SBP() {
+		if (m_jButton_Search_SBP == null) {
+			m_jButton_Search_SBP = new JButton();
+			m_jButton_Search_SBP.setBounds(new Rectangle(396, 480, 208, 34));
+			m_jButton_Search_SBP.setText("Search Book");
+			m_jButton_Search_SBP.addActionListener(this);
+		}
+		return m_jButton_Search_SBP;
 	}
 
 }
