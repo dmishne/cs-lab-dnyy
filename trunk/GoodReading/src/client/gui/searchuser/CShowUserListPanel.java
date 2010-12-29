d884ece6e9ae100242e2fabe46b25d1e949f30da
evgeny11183 <evgeny11183@ee6e6aaf-207a-3dc3-f7a4-2c6dd9a5b256>
2010-12-29 15:53:34 +0000

    CLibraryManager , CSearchUserPanel , CUser : Some API and extensions changes gui:   searchuser: Added "CShowUserPanel", "CUserDetailsPanel" (may contain some bugs on "Save" action... can't check now)

---
+++ trunk/GoodReading/src/client/gui/searchuser/CShowUserListPanel.java
@@ -0,0 +1,200 @@
+package client.gui.searchuser;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.ComponentOrientation;
+import java.awt.Point;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.LinkedList;
+import javax.swing.BorderFactory;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
+import common.data.CUser;
+import javax.swing.JButton;
+
+public class CShowUserListPanel extends JPanel implements ActionListener{
+
+	private static final long serialVersionUID = 1L;
+	private JLabel jLabel_main_label = null;
+	private JScrollPane jScrollPane_UL = null;
+	private JList jList_UL = null;
+	private JButton jButton_back_UL = null;
+	private JButton jButton_ShowUserDetails_UL = null;
+	private static CUser m_chosenUser = null;
+	private ULPDecision m_lastChoice = null;
+	private LinkedList<CUser> usersList = null;
+
+	
+	
+	public enum ULPDecision
+	{
+		BACK,USERLIST
+	}
+	/**
+	 * This is the default constructor
+	 */
+	public CShowUserListPanel() throws Exception{
+		super();
+		initialize();
+	}
+
+	/**
+	 * This method initializes this
+	 * 
+	 * @return void
+	 */
+	private void initialize() throws Exception{
+		jLabel_main_label = new JLabel();
+		jLabel_main_label.setText("Users Result List");
+		jLabel_main_label.setLocation(new Point(0, 0));
+		jLabel_main_label.setHorizontalAlignment(SwingConstants.CENTER);
+		jLabel_main_label.setFont(new Font("Freestyle Script", Font.BOLD, 36));
+		jLabel_main_label.setSize(new Dimension(700, 35));
+		this.setSize(700, 600);
+		this.setLayout(null);
+		this.add(jLabel_main_label, null);
+		this.add(getJScrollPane_UL(), null);
+		this.add(getJButton_back_UL(), null);
+		this.add(getJButton_ShowUserDetails_UL(), null);
+	}
+
+	
+	/**
+	 * @param m_lastChoice the m_lastChoice to set
+	 */
+	public void setLastChoice(ULPDecision m_lastChoice) {
+		this.m_lastChoice = m_lastChoice;
+	}
+
+	/**
+	 * @return the m_lastChoice
+	 */
+	public ULPDecision getLastChoice() {
+		return m_lastChoice;
+	}
+
+	
+	
+	/**
+	 * @return the m_chosenUser
+	 */
+	static public CUser getChosenUser() {
+		return m_chosenUser;
+	}
+
+	/**
+	 * This method initializes jScrollPane_UL	
+	 * 	
+	 * @return javax.swing.JScrollPane	
+	 */
+	private JScrollPane getJScrollPane_UL() throws Exception{
+		if (jScrollPane_UL == null) {
+			jScrollPane_UL = new JScrollPane();
+			jScrollPane_UL.setLocation(new Point(50, 50));
+			jScrollPane_UL.setViewportView(getJList_UL());
+			jScrollPane_UL.setSize(new Dimension(600, 400));
+			jScrollPane_UL.setBorder(BorderFactory.createLineBorder(Color.black));
+		}
+		return jScrollPane_UL;
+	}
+
+	/**
+	 * This method initializes jList_UL	
+	 * 	
+	 * @return javax.swing.JList	
+	 */
+	private JList getJList_UL() throws Exception{
+		if (jList_UL == null) {
+			usersList = CSearchUserPanel.getResult();
+			String []users = new String[usersList.size()];
+			int i = 0;
+			for( CUser user : usersList )
+			{
+				users[i] = user.getM_userName() + " - " + user.getM_userID() + " - " + user.getM_firstName() + " - " + user.getM_lastName();
+				i++;
+			}
+			jList_UL = new JList(users);
+			jList_UL.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			jList_UL.setSize(new Dimension(600, 400));
+			jList_UL.setLocation(new Point(50, 50));
+			jList_UL.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		}
+		return jList_UL;
+	}
+
+	/**
+	 * This method initializes jButton_back_UL	
+	 * 	
+	 * @return javax.swing.JButton	
+	 */
+	private JButton getJButton_back_UL() {
+		if (jButton_back_UL == null) {
+			jButton_back_UL = new JButton();
+			jButton_back_UL.setLocation(new Point(94, 480));
+			jButton_back_UL.setText("Back");
+			jButton_back_UL.setSize(new Dimension(208, 34));
+			jButton_back_UL.addActionListener(this);
+		}
+		return jButton_back_UL;
+	}
+
+	/**
+	 * This method initializes jButton_ShowUserDetails_UL	
+	 * 	
+	 * @return javax.swing.JButton	
+	 */
+	private JButton getJButton_ShowUserDetails_UL() {
+		if (jButton_ShowUserDetails_UL == null) {
+			jButton_ShowUserDetails_UL = new JButton();
+			jButton_ShowUserDetails_UL.setLocation(new Point(396, 480));
+			jButton_ShowUserDetails_UL.setText("Show User Details");
+			jButton_ShowUserDetails_UL.setSize(new Dimension(208, 34));
+			jButton_ShowUserDetails_UL.addActionListener(this);
+		}
+		return jButton_ShowUserDetails_UL;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		Object source = ae.getSource();
+		if(source == jButton_back_UL)
+		{
+			this.setLastChoice(ULPDecision.BACK);
+			this.setVisible(false);
+		}
+		if(source == jButton_ShowUserDetails_UL)
+		{
+			try{
+				String res = (String)jList_UL.getSelectedValue();
+				String[] splitedRes = res.split(" - ");
+				Iterator<CUser> it = usersList.iterator();
+				while(it.hasNext())
+				{
+					CUser temp = it.next();
+					if(temp.getM_userName().compareTo(splitedRes[0]) == 0)
+					{
+						m_chosenUser = temp;
+						break;
+					}
+				}
+				this.m_lastChoice = ULPDecision.USERLIST;
+				this.setVisible(false);
+			}
+			catch (Exception e)
+			{
+				JOptionPane.showMessageDialog(null, "Please choose user from list" ,"Error",JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		
+	}
+
+	
+}
---
+++ trunk/GoodReading/src/client/gui/searchuser/CUserDetailsPanel.java
@@ -0,0 +1,540 @@
+package client.gui.searchuser;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import javax.swing.JButton;
+
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.awt.Point;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.peer.ChoicePeer;
+
+import javax.swing.BorderFactory;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
+import javax.swing.JMenu;
+
+import client.core.AUser;
+import client.core.CLibraryManager;
+import client.core.EActor;
+import client.gui.searchbook.CBookDetailPanel.EBDDecision;
+
+import common.data.CUser;
+
+
+public class CUserDetailsPanel extends JPanel implements ActionListener{
+
+	private static final long serialVersionUID = 1L;
+	private JButton jButton_Edit_UD = null;
+	private JButton jButton_Back_UD = null;
+	private JButton jButton_showReport_UD = null;
+	private JButton jButton_Save_UD = null;
+	private JLabel jLabel_mainLabel = null;
+	private JLabel jLabel_userName = null;
+	private JLabel jLabel_UserID = null;
+	private JLabel jLabel_firstName = null;
+	private JLabel jLabel_lastName = null;
+	private JLabel jLabel_Privilage = null;
+	private JLabel jLabel_suspend = null;
+	private JLabel jLabel_payType = null;
+	private JLabel jLabel_adress = null;
+	private JLabel jLabel_birthDay = null;
+	private JTextField jTextField_userName = null;
+	private JTextField jTextField_userID = null;
+	private JTextField jTextField_firstName = null;
+	private JTextField jTextField_lastName = null;
+	private JTextField jTextField_adress = null;
+	private JTextField jTextField_birthDay = null;
+	private JComboBox jComboBox_privilage = null;
+	private JCheckBox jCheckBox_yearly = null;
+	private JCheckBox jCheckBox_monthly = null;
+	private JCheckBox jCheckBox_creditCard = null;
+	private JLabel jLabel_yearly = null;
+	private JLabel jLabel_monthly = null;
+	private JLabel jLabel_creditCard = null;
+	private JCheckBox jCheckBox_suspend = null;
+	private boolean edit_flag;
+	private CUser chosenUser=null;
+	private UDDecision m_lastChoice = null;
+	
+	
+	public enum UDDecision
+	{
+		BACK,SHOWREPORT,SAVE
+	}
+	
+	/**
+	 * This is the default constructor
+	 */
+	public CUserDetailsPanel() {
+		super();
+		chosenUser = CShowUserListPanel.getChosenUser();
+		initialize();
+	}
+
+	/**
+	 * This method initializes this
+	 * 
+	 * @return void
+	 */
+	private void initialize() {
+		jLabel_creditCard = new JLabel();
+		jLabel_creditCard.setText("Credit Card");
+		jLabel_creditCard.setSize(new Dimension(80, 30));
+		jLabel_creditCard.setLocation(new Point(120, 360));
+		jLabel_monthly = new JLabel();
+		jLabel_monthly.setText("Monthly");
+		jLabel_monthly.setSize(new Dimension(80, 30));
+		jLabel_monthly.setLocation(new Point(120, 320));
+		jLabel_yearly = new JLabel();
+		jLabel_yearly.setText("Yearly");
+		jLabel_yearly.setSize(new Dimension(80, 30));
+		jLabel_yearly.setLocation(new Point(120, 280));
+		jLabel_birthDay = new JLabel();
+		jLabel_birthDay.setText("Birth Day");
+		jLabel_birthDay.setSize(new Dimension(80, 30));
+		jLabel_birthDay.setLocation(new Point(40, 230));
+		jLabel_adress = new JLabel();
+		jLabel_adress.setText("Adress");
+		jLabel_adress.setSize(new Dimension(80, 30));
+		jLabel_adress.setLocation(new Point(40, 180));
+		jLabel_payType = new JLabel();
+		jLabel_payType.setText("Pay Type :");
+		jLabel_payType.setSize(new Dimension(80, 30));
+		jLabel_payType.setLocation(new Point(40, 280));
+		jLabel_suspend = new JLabel();
+		jLabel_suspend.setText("Suspend");
+		jLabel_suspend.setLocation(new Point(485, 360));
+		jLabel_suspend.setSize(new Dimension(111, 30));
+		jLabel_suspend.setBorder(BorderFactory.createLineBorder(Color.black));
+		jLabel_Privilage = new JLabel();
+		jLabel_Privilage.setText("Privilage");
+		jLabel_Privilage.setLocation(new Point(375, 230));
+		jLabel_Privilage.setSize(new Dimension(80, 30));
+		jLabel_lastName = new JLabel();
+		jLabel_lastName.setText("Last Name");
+		jLabel_lastName.setLocation(new Point(375, 130));
+		jLabel_lastName.setSize(new Dimension(80, 30));
+		jLabel_firstName = new JLabel();
+		jLabel_firstName.setText("First Name");
+		jLabel_firstName.setLocation(new Point(40, 130));
+		jLabel_firstName.setSize(new Dimension(80, 30));
+		jLabel_UserID = new JLabel();
+		jLabel_UserID.setText("User ID");
+		jLabel_UserID.setLocation(new Point(375, 80));
+		jLabel_UserID.setSize(new Dimension(80, 30));
+		jLabel_userName = new JLabel();
+		jLabel_userName.setText("User Name");
+		jLabel_userName.setLocation(new Point(40, 80));
+		jLabel_userName.setSize(new Dimension(80, 30));
+		jLabel_mainLabel = new JLabel();
+		jLabel_mainLabel.setText("User Details");
+		jLabel_mainLabel.setLocation(new Point(0, 0));
+		jLabel_mainLabel.setPreferredSize(new Dimension(700, 35));
+		jLabel_mainLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		jLabel_mainLabel.setFont(new Font("Freestyle Script", Font.BOLD, 36));
+		jLabel_mainLabel.setSize(new Dimension(700, 35));
+		edit_flag = Boolean.FALSE;
+		this.setSize(700, 600);
+		this.setLayout(null);
+		this.add(getJButton_Edit_UD(), null);
+		this.add(getJButton_Back_UD(), null);
+		this.add(getJButton_showReport_UD(), null);
+		this.add(getJButton_Save_UD(), null);
+		this.add(jLabel_mainLabel, null);
+		this.add(jLabel_userName, null);
+		this.add(jLabel_UserID, null);
+		this.add(jLabel_firstName, null);
+		this.add(jLabel_lastName, null);
+		this.add(jLabel_Privilage, null);
+		this.add(jLabel_suspend, null);
+		this.add(jLabel_payType, null);
+		this.add(jLabel_adress, null);
+		this.add(jLabel_birthDay, null);
+		this.add(getJTextField_userName(), null);
+		this.add(getJTextField_userID(), null);
+		this.add(getJTextField_firstName(), null);
+		this.add(getJTextField_lastName(), null);
+		this.add(getJTextField_adress(), null);
+		this.add(getJTextField_birthDay(), null);
+		this.add(getJComboBox_privilage(), null);
+		this.add(getJCheckBox_yearly(), null);
+		this.add(getJCheckBox_monthly(), null);
+		this.add(getJCheckBox_creditCard(), null);
+		this.add(jLabel_yearly, null);
+		this.add(jLabel_monthly, null);
+		this.add(jLabel_creditCard, null);
+		this.add(getJCheckBox_suspend(), null);
+	}
+
+	/**
+	 * @return the m_lastChoice
+	 */
+	public UDDecision getLastChoice() {
+		return m_lastChoice;
+	}
+
+	/**
+	 * @param m_lastChoice the m_lastChoice to set
+	 */
+	public void setLastChoice(UDDecision m_lastChoice) {
+		this.m_lastChoice = m_lastChoice;
+	}
+
+	/**
+	 * This method initializes jButton_Edit_UD	
+	 * 	
+	 * @return javax.swing.JButton	
+	 */
+	private JButton getJButton_Edit_UD() {
+		if (jButton_Edit_UD == null) {
+			jButton_Edit_UD = new JButton();
+			jButton_Edit_UD.setText("Edit");
+			jButton_Edit_UD.setSize(new Dimension(130, 34));
+			jButton_Edit_UD.setLocation(new Point(202, 500));
+			jButton_Edit_UD.addActionListener(this);
+		}
+		return jButton_Edit_UD;
+	}
+
+	/**
+	 * This method initializes jButton_Back_UD	
+	 * 	
+	 * @return javax.swing.JButton	
+	 */
+	private JButton getJButton_Back_UD() {
+		if (jButton_Back_UD == null) {
+			jButton_Back_UD = new JButton();
+			jButton_Back_UD.setText("Back");
+			jButton_Back_UD.setSize(new Dimension(130, 34));
+			jButton_Back_UD.setLocation(new Point(36, 500));
+			jButton_Back_UD.addActionListener(this);
+		}
+		return jButton_Back_UD;
+	}
+
+	/**
+	 * This method initializes jButton_showReport_UD	
+	 * 	
+	 * @return javax.swing.JButton	
+	 */
+	private JButton getJButton_showReport_UD() {
+		if (jButton_showReport_UD == null) {
+			jButton_showReport_UD = new JButton();
+			jButton_showReport_UD.setText("Show Report");
+			jButton_showReport_UD.setSize(new Dimension(130, 34));
+			jButton_showReport_UD.setLocation(new Point(368, 500));
+			jButton_showReport_UD.addActionListener(this);
+		}
+		return jButton_showReport_UD;
+	}
+
+	/**
+	 * This method initializes jButton_Save_UD	
+	 * 	
+	 * @return javax.swing.JButton	
+	 */
+	private JButton getJButton_Save_UD() {
+		if (jButton_Save_UD == null) {
+			jButton_Save_UD = new JButton();
+			jButton_Save_UD.setText("Save");
+			jButton_Save_UD.setSize(new Dimension(130, 34));
+			jButton_Save_UD.setLocation(new Point(534, 500));
+			jButton_Save_UD.addActionListener(this);
+		}
+		return jButton_Save_UD;
+	}
+
+	/**
+	 * This method initializes jTextField_userName	
+	 * 	
+	 * @return javax.swing.JTextField	
+	 */
+	private JTextField getJTextField_userName() {
+		if (jTextField_userName == null) {
+			jTextField_userName = new JTextField();
+			jTextField_userName.setEditable(false);
+			jTextField_userName.setSize(new Dimension(200, 20));
+			jTextField_userName.setLocation(new Point(120, 85));
+			jTextField_userName.setText("Best Play");  // stub
+			//jTextField_userName.setText(chosenUser.getM_userName());
+		}
+		return jTextField_userName;
+	}
+
+	/**
+	 * This method initializes jTextField_userID	
+	 * 	
+	 * @return javax.swing.JTextField	
+	 */
+	private JTextField getJTextField_userID() {
+		if (jTextField_userID == null) {
+			jTextField_userID = new JTextField();
+			jTextField_userID.setEditable(false);
+			jTextField_userID.setSize(new Dimension(200, 20));
+			jTextField_userID.setLocation(new Point(455, 85));
+			//String userid = Integer.toString(chosenUser.getM_userID());
+			//jTextField_userID.setText(userid);
+			jTextField_userID.setText("3765417"); // stub
+		}
+		return jTextField_userID;
+	}
+
+	/**
+	 * This method initializes jTextField_firstName	
+	 * 	
+	 * @return javax.swing.JTextField	
+	 */
+	private JTextField getJTextField_firstName() {
+		if (jTextField_firstName == null) {
+			jTextField_firstName = new JTextField();
+			jTextField_firstName.setEditable(false);
+			jTextField_firstName.setSize(new Dimension(200, 20));
+			jTextField_firstName.setLocation(new Point(120, 135));
+			//jTextField_firstName.setText(chosenUser.getM_firstName());
+			jTextField_firstName.setText("Daniel"); // stub
+		}
+		return jTextField_firstName;
+	}
+
+	/**
+	 * This method initializes jTextField_lastName	
+	 * 	
+	 * @return javax.swing.JTextField	
+	 */
+	private JTextField getJTextField_lastName() {
+		if (jTextField_lastName == null) {
+			jTextField_lastName = new JTextField();
+			jTextField_lastName.setEditable(false);
+			jTextField_lastName.setLocation(new Point(455, 135));
+			jTextField_lastName.setSize(new Dimension(200, 20));
+			//jTextField_lastName.setText(chosenUser.getM_lastName());
+			jTextField_lastName.setText("Mishne"); // stub
+		}
+		return jTextField_lastName;
+	}
+
+	/**
+	 * This method initializes jTextField_adress	
+	 * 	
+	 * @return javax.swing.JTextField	
+	 */
+	private JTextField getJTextField_adress() {
+		if (jTextField_adress == null) {
+			jTextField_adress = new JTextField();
+			jTextField_adress.setEditable(false);
+			jTextField_adress.setSize(new Dimension(535, 20));
+			jTextField_adress.setLocation(new Point(120, 185));
+			//jTextField_adress.setText(chosenUser.getAdress());
+			jTextField_adress.setText("Asif 58/29, Gvat Ram, Carmiel"); // stub
+		}
+		return jTextField_adress;
+	}
+
+	/**
+	 * This method initializes jTextField_birthDay	
+	 * 	
+	 * @return javax.swing.JTextField	
+	 */
+	private JTextField getJTextField_birthDay() {
+		if (jTextField_birthDay == null) {
+			jTextField_birthDay = new JTextField();
+			jTextField_birthDay.setEditable(false);
+			jTextField_birthDay.setSize(new Dimension(200, 20));
+			jTextField_birthDay.setLocation(new Point(120, 235));
+			//jTextField_birthDay.setText(chosenUser.getBirthDay());
+			jTextField_birthDay.setText("15.09.1985"); // stub
+		}
+		return jTextField_birthDay;
+	}
+
+	/**
+	 * This method initializes jComboBox_privilage	
+	 * 	
+	 * @return javax.swing.JComboBox	
+	 */
+	private JComboBox getJComboBox_privilage() {
+		if (jComboBox_privilage == null) {
+			String[] privilagesToShow = new String[4];
+			String[] privilage = {EActor.Reader.toString(),EActor.User.toString(),EActor.Librarian.toString(),EActor.LibraryManager.toString()};
+			privilagesToShow[0] = "User";  // stub
+			//privilagesToShow[0] = chosenUser.getM_privilege().toString(); 
+			int i = 1;
+			for(String pr : privilage)
+			{
+				if( privilagesToShow[0].compareTo(pr) != 0)
+				{
+					privilagesToShow[i]=pr;
+				    i++;
+				}
+			}
+			jComboBox_privilage = new JComboBox(privilagesToShow);
+			jComboBox_privilage.setEnabled(false);
+			jComboBox_privilage.setSize(new Dimension(200, 20));
+			jComboBox_privilage.setLocation(new Point(455, 235));		
+		}
+		return jComboBox_privilage;
+	}
+
+	/**
+	 * This method initializes jCheckBox_yearly	
+	 * 	
+	 * @return javax.swing.JCheckBox	
+	 */
+	private JCheckBox getJCheckBox_yearly() {
+		if (jCheckBox_yearly == null) {
+			jCheckBox_yearly = new JCheckBox();
+			jCheckBox_yearly.setSize(new Dimension(23, 20));
+			jCheckBox_yearly.setEnabled(false);
+			jCheckBox_yearly.setLocation(new Point(210, 287));
+			//for(String pt : chosenUser.getPayTypes())
+			  // if(pt.compareTo("Yearly") == 0)
+				   jCheckBox_yearly.setSelected(true);
+		}
+		return jCheckBox_yearly;
+	}
+
+	/**
+	 * This method initializes jCheckBox_monthly	
+	 * 	
+	 * @return javax.swing.JCheckBox	
+	 */
+	private JCheckBox getJCheckBox_monthly() {
+		if (jCheckBox_monthly == null) {
+			jCheckBox_monthly = new JCheckBox();
+			jCheckBox_monthly.setSize(new Dimension(17, 17));
+			jCheckBox_monthly.setEnabled(false);
+			jCheckBox_monthly.setLocation(new Point(210, 327));
+		//	for(String pt : chosenUser.getPayTypes())
+			//	   if(pt.compareTo("Monthly") == 0)
+				//	   jCheckBox_monthly.setSelected(true);
+		}
+		return jCheckBox_monthly;
+	}
+
+	/**
+	 * This method initializes jCheckBox_creditCard	
+	 * 	
+	 * @return javax.swing.JCheckBox	
+	 */
+	private JCheckBox getJCheckBox_creditCard() {
+		if (jCheckBox_creditCard == null) {
+			jCheckBox_creditCard = new JCheckBox();
+			jCheckBox_creditCard.setSize(new Dimension(22, 16));
+			jCheckBox_creditCard.setEnabled(false);
+			jCheckBox_creditCard.setLocation(new Point(210, 367));
+			//for(String pt : chosenUser.getPayTypes())
+				//   if(pt.compareTo("CreditCard") == 0)
+					   jCheckBox_creditCard.setSelected(true);
+		}
+		return jCheckBox_creditCard;
+	}
+
+	/**
+	 * This method initializes jCheckBox_suspend	
+	 * 	
+	 * @return javax.swing.JCheckBox	
+	 */
+	private JCheckBox getJCheckBox_suspend() {
+		if (jCheckBox_suspend == null) {
+			jCheckBox_suspend = new JCheckBox();
+			jCheckBox_suspend.setSize(new Dimension(17, 15));
+			jCheckBox_suspend.setEnabled(false);
+			jCheckBox_suspend.setFont(new Font("Freestyle Script", Font.BOLD, 12));
+			jCheckBox_suspend.setForeground(Color.red);
+			jCheckBox_suspend.setText("");
+			jCheckBox_suspend.setLocation(new Point(565, 367));
+			//if(chosenUser.isSuspend())
+				//jCheckBox_suspend.setSelected(true);
+		}
+		return jCheckBox_suspend;
+	}
+
+	
+	public void actionPerformed(ActionEvent ae) {
+		Object source = ae.getSource();
+		if(source == jButton_Back_UD)
+		{
+			this.setLastChoice(UDDecision.BACK);
+			this.setVisible(false);
+		}
+		if(source == jButton_Edit_UD)
+		{
+			if(!edit_flag)
+			{
+				jTextField_userName.setEditable(true);
+				jCheckBox_suspend.setEnabled(true);
+			//	String [] pt = chosenUser.getPayTypes();
+			//	for(String cc : pt)
+			//		if(cc.compareTo("CreditCard") == 0)
+						jCheckBox_creditCard.setEnabled(true);				
+				jCheckBox_monthly.setEnabled(true);
+				jCheckBox_yearly.setEnabled(true);
+				jComboBox_privilage.setEnabled(true);
+				jTextField_birthDay.setEditable(true);
+				jTextField_adress.setEditable(true);
+				jTextField_lastName.setEditable(true);
+				jTextField_firstName.setEditable(true);
+				jTextField_userID.setEditable(true);
+				jTextField_userName.setEditable(true);
+				edit_flag = Boolean.TRUE;
+			}
+			else if(edit_flag)
+			{
+				jTextField_userName.setEditable(false);
+				jCheckBox_suspend.setEnabled(false);
+				jCheckBox_creditCard.setEnabled(false);
+				jCheckBox_monthly.setEnabled(false);
+				jCheckBox_yearly.setEnabled(false);
+				jComboBox_privilage.setEnabled(false);
+				jTextField_birthDay.setEditable(false);
+				jTextField_adress.setEditable(false);
+				jTextField_lastName.setEditable(false);
+				jTextField_firstName.setEditable(false);
+				jTextField_userID.setEditable(false);
+				jTextField_userName.setEditable(false);
+				edit_flag = Boolean.FALSE;
+			}
+		}
+		if(source == jButton_showReport_UD)
+		{
+			this.setLastChoice(UDDecision.SHOWREPORT);
+			this.setVisible(false);
+		}
+		if(source == jButton_Save_UD)
+		{
+			String[] paytypes = new String[3];
+			EActor privilage = EActor.User;
+			if(jCheckBox_yearly.isSelected())
+				paytypes[0] = "Yearly";
+			if(jCheckBox_monthly.isSelected())
+				paytypes[1] = "Monthly";
+			if(jCheckBox_creditCard.isSelected())
+				paytypes[2] = "CreditCard";
+		    if(jComboBox_privilage.getSelectedItem().toString() == "Reader")
+				privilage = EActor.Reader;
+			else if(jComboBox_privilage.getSelectedItem().toString() == "Library Manager")
+				privilage = EActor.LibraryManager;
+			else if(jComboBox_privilage.getSelectedItem().toString() == "Librarian")
+				privilage = EActor.Librarian;			
+				try {
+					((CLibraryManager)AUser.getInstance()).updateUserDetails(chosenUser.getM_userName(), jTextField_firstName.getText(), jTextField_lastName.getText(), jTextField_userName.getText(), Integer.parseInt(jTextField_userID.getText()), jTextField_birthDay.getText(), jTextField_adress.getText(), paytypes, privilage, jCheckBox_suspend.isSelected());
+				} catch (NumberFormatException e) {
+					System.out.println("Can't convert from String to Integer");
+				} catch (Exception e) {
+					System.out.println("Fail to reach client.core.CLibraryManager.updateUserDetails");
+				}
+			this.setLastChoice(UDDecision.SAVE);
+			this.setVisible(false);
+		}
+		
+	}
+
+}
