74f04d38f47fb9cd307b21917673d2aeb35d925d
geffen.nir <geffen.nir@ee6e6aaf-207a-3dc3-f7a4-2c6dd9a5b256>
2011-01-10 21:43:29 +0000

    built infrastracture for Popularity check 	Added stubs at dbig 	Added Rank attr to CBook 	Added functions to CExecuter
    	these are not checked - will wait untill tmr, plz don't use the new functions.

--- trunk/GoodReading/src/common/data/CBook.java
+++ trunk/GoodReading/src/common/data/CBook.java
@@ -6,24 +6,24 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class CBook implements Serializable {
+public class CBook implements Serializable,Comparable {
 	
 	static private final long serialVersionUID = 1L;
-	private String  m_title;
-	private String  m_author;
-	private String m_ISBN;
-	private String m_release;    // Warning : Date type used
-	private String m_publisher;
-	private String m_summary;
-	private double m_price;
-	private double m_score=0;
-	private long m_score_count=0;
-	private String m_topic;
-	//private String m_subtopic;
-	private String m_lables;
-	private String m_TOC;
-	private boolean m_invisible;
-	private String m_language;
+	private String	 m_title;
+	private String	 m_author;
+	private String	 m_ISBN;
+	private String	 m_release;    // Warning : Date type used
+	private String	 m_publisher;
+	private String	 m_summary;
+	private double	 m_price;
+	private double	 m_score=0;
+	private long 	 m_score_count=0;
+	private String	 m_topic;
+	private int		 m_rank;
+	private String	 m_lables;
+	private String	 m_TOC;
+	private boolean	 m_invisible;
+	private String	 m_language;
 	
 	/**
 	 * @param m_title
@@ -43,7 +43,7 @@ public class CBook implements Serializable {
 	 * @param m_release_date 
 	 */
 	
-	public CBook(String m_ISBN, String m_author, String m_title, String m_release, String m_publisher, String m_summary,double m_price, long m_score_count,double m_score,String m_topic, String m_lables, String m_TOC,boolean m_invisible, String m_language, String m_subtopic) 
+	public CBook(String m_ISBN, String m_author, String m_title, String m_release, String m_publisher, String m_summary,double m_price, long m_score_count,double m_score,String m_topic, String m_lables, String m_TOC,boolean m_invisible, String m_language,int Rank) 
 	{   
 		this.m_title = m_title;
 		this.m_author = m_author;
@@ -53,16 +53,24 @@ public class CBook implements Serializable {
 		this.m_summary = m_summary;
 		this.m_price = m_price;
 		this.m_topic = m_topic;
-	//	this.m_subtopic = m_subtopic;
 		this.m_lables = m_lables;
 		this.m_TOC = m_TOC;
 		this.m_invisible = m_invisible;
 		this.m_language = m_language;
 		this.m_score_count=m_score_count;
 		this.m_score= m_score;
+		this.m_rank=Rank;
 	}
 
-	
+	public int getM_rank()
+	{
+		return m_rank;
+	}
+
+	public void setM_rank(int r)
+	{
+		m_rank=r;
+	}
 	
 	/**
 	 * @return the m_title
@@ -279,4 +287,17 @@ public class CBook implements Serializable {
 	}
 	
 	*/
+
+
+//this function is only supposed to compare between CBooks, not anything else!
+//implements the Comparable interface
+	public int compareTo(Object arg0) 
+	{
+		CBook b=(CBook) arg0;
+		if(m_rank>b.m_rank)
+			return 1;
+		if(m_rank<b.m_rank)
+			return -1;
+		return m_ISBN.compareTo(b.m_ISBN);
+	}
 }
