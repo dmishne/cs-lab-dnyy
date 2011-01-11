c074ff599eaf5261cfa81d5ecb6042d7a51621ee
daniel.mishne <daniel.mishne@ee6e6aaf-207a-3dc3-f7a4-2c6dd9a5b256>
2011-01-11 13:52:23 +0000

    CClientConnector - Javadoc completed.

--- trunk/GoodReading/src/client/common/CClientConnector.java
+++ trunk/GoodReading/src/client/common/CClientConnector.java
@@ -6,17 +6,48 @@ import common.api.CListOptions;
 
 import ocsf.client.AbstractClient;
 
+/**
+ * CClientConnector holds the client's application connection information
+ * CClientConnector is a Singleton
+ */
 public class CClientConnector extends AbstractClient {
 		
-
+		/**
+		 * Instance of class
+		 */
 		private static CClientConnector m_ConnectorInstance = null;
+		/**
+		 * Clients session ID
+		 * Users to identify in front of server
+		 */
 		private int m_clientSessionID;
+		/**
+		 * Indicates whether there is a message waiting
+		 */
 		private boolean m_haveMsg;
+		/**
+		 * Store the last received message
+		 */
 		private Object m_msg;
+		/**
+		 * Host of Server
+		 */
 		private static String m_host = "localhost";
+		/**
+		 * Indicates whether Server host has been changes
+		 */
 		private static boolean m_change;
+		/**
+		 * Save list of available book topic and languages in database
+		 */
 		private CListOptions m_listOptions;
-				
+		
+		/**
+		 * Hidden Constructor,
+		 * Initialize singleton instance, receiving and saving Books's topics list.   
+		 * @param host Server's address xxx.xxx.xxx.xxx
+		 * @param port Server's port xxxx
+		 */
 		private CClientConnector(String host, int port)
 		{
 			super(host,port);
@@ -25,6 +56,11 @@ public class CClientConnector extends AbstractClient {
 			this. m_listOptions = CListOptions.getInstance(); 
 		}
 		
+		/**
+		 * getInstance return an instance of CClientConnect and open connection to server.
+		 * @return An instance of CClientConnector.
+		 * @throws Exception When can't connect to server.
+		 */
 		public static CClientConnector getInstance() throws Exception
 		{
 			if(m_ConnectorInstance == null || m_change == true)
@@ -36,12 +72,22 @@ public class CClientConnector extends AbstractClient {
 			return m_ConnectorInstance;
 		}
 
+		/**
+		 * setConnectionHost sets the server's address.
+		 * @param host Server's address xxx.xxx.xxx.xxx
+		 */
 		public static void setConnectionHost(String host)
 		{
 			m_host = host;
 			m_change = true;
 		}
 		
+		/**
+		 * handleMessageFromServer overload a method from AbstractClient.
+		 * handleMessageFromServer handle the message that received from the server
+		 * by storing the message and informs that the message has received.
+		 * @param message message from server.
+		 */
 		final protected void handleMessageFromServer(Object message)
 		{
 			     // if message exist, the new message will overwrite it
@@ -53,6 +99,12 @@ public class CClientConnector extends AbstractClient {
 			     }
 		}
 		
+		/**
+		 * messageToServer sends message to server and return the message received from server.
+		 * messageToServer will wait till it get response message from server. 
+		 * @param message Message that should be passed to the server.
+		 * @return The response from to the server to the message to the client.
+		 */
 		public Object messageToServer(Object message)
 		{
 		   //send to server
@@ -71,7 +123,6 @@ public class CClientConnector extends AbstractClient {
 			catch (InterruptedException e) { 
 				e.printStackTrace();
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.getCause();
 			} 				
 			return (null);	
@@ -84,40 +135,40 @@ public class CClientConnector extends AbstractClient {
 			return(m_haveMsg);	
 		}
 		
-		
 		/**
-		 * @return the m_msg
+		 * @return The Message
 		 */
 		public Object getMsg() {
 			m_haveMsg = false;
 			return m_msg;
 		}
-		
-		
+				
 		/**
-		 * @return the m_clientSessionID
+		 * @return The client session ID
 		 */
 		public int getM_clientSessionID() {
 			return m_clientSessionID;
 		}
 
-		
 		/**
-		 * @return the m_listOptions
+		 * @return The listOptions
 		 */
 		public CListOptions getM_listOptions() {
 			return m_listOptions;
 		}
 		
-		
 		/**
-		 * @param m_clientSessionID the m_clientSessionID to set
+		 * setM_clientSessionID setting client session ID
+		 * @param m_clientSessionID client session ID
 		 */
 		public void setM_clientSessionID(int m_clientSessionID) {
 			this.m_clientSessionID = m_clientSessionID;
 		}
 
-		
+		/**
+		 * getLangages returns Available book's languages in server 
+		 * @return Available book's languages in server 
+		 */
 		public String[] getLangages()
 		{
 			String[] lang = new String[ m_listOptions.getM_langueges().size() + 1];
@@ -133,9 +184,10 @@ public class CClientConnector extends AbstractClient {
 			return lang;
 		}
 		
-		
-		
-
+		/**
+		 * getTopics returns Available book's topics in server 
+		 * @return Available book's topics in server
+		 */
 		public String[] getTopics()
 		{
 			String[] topics = new String[ m_listOptions.getM_topics().size() + 1];
@@ -147,10 +199,7 @@ public class CClientConnector extends AbstractClient {
 				topics[i] = it.next();
 				i++;
 			}
-			//String[] topics = {" ","topic11","Internet","History"};
 			return topics;
 		}
-
-
+		
 }
-
