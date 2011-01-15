83fa73c0f0c32ef424f307d218b59ccaf49ea91c
evgeny11183 <evgeny11183@ee6e6aaf-207a-3dc3-f7a4-2c6dd9a5b256>
2011-01-15 12:12:42 +0000

    Gui label correction

--- trunk/GoodReading/src/client/gui/addnewbook/CAddNewBookPanel.java
+++ trunk/GoodReading/src/client/gui/addnewbook/CAddNewBookPanel.java
@@ -129,7 +129,7 @@ public class CAddNewBookPanel extends JPanel implements ActionListener{
                 jLabel_label.setFont(new Font("Eras Light ITC", Font.BOLD, 12));
                 jLabel_label.setSize(new Dimension(90, 26));
                 jLabel_label.setLocation(new Point(20, 248));
-                jLabel_label.setText("Labels");
+                jLabel_label.setText("Labels :");
                 jLabel_toc = new JLabel();
                 jLabel_toc.setText("Table Of Contents");
                 jLabel_toc.setLocation(new Point(476, 185));
